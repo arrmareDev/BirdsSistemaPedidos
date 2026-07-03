df657d027b29de19aa7422cdea2c4bbb69efa5a4
chuquidev <jchuquipomajimenez@gmail.com>
2026-07-03 11:23:45 -0500

    correcion de import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 import { createHead } from "@vueuse/head";
 import App from "./App.vue";
 import router from "./router";
-import "../assets/main.css";
+import "./assets/main.css";
 
 const app = createApp(App);
 const head = createHead();
